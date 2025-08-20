2b07cb5f216edd33844099594fab9c8f2249bc14
Justin B. <jbryant9059@floridapoly.edu>
2025-08-20 18:41:43 +0000

    Created unix instructions

---
+++ 01_c++_intro/first_program.cpp
@@ -0,0 +1,24 @@
+/*
+    20/08/25
+    The first C++ program
+*/
+
+#include <iostream>
+
+using namespace std;
+
+using std::cout;
+
+// Create a main function
+int main(void)
+{
+    std::cout << " Hello COP3337!" << std::endl;
+
+    // std - standard namespace
+    // :: - scope resolution operator
+    // cout - console output (object of an iostream class)
+    // << - stream insertion operator
+    // endl - end of line
+
+    return 0;
+}
