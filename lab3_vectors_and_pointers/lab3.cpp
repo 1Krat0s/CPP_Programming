1c94eb4f4ec2d3753b3d09cb165e042ea889589e
Justin B. <jbryant9059@floridapoly.edu>
2025-09-15 18:44:50 +0000

    Completed Lab 3

---
+++ lab3_vectors_and_pointers/lab3.cpp
@@ -0,0 +1,68 @@
+// Question 1
+#include <iostream>
+#include <vector>
+
+int numberOfDuplicates(const std::vector<int>& nums);
+void printVector(const std::vector<int>& nums);
+
+int numberOfDuplicates(const std::vector<int>& nums)
+{
+    std::vector<int> duplicates;
+    for (int i = 0; nums.size(); i++)
+    {
+        bool isDuplicate = false;
+        
+        for (int j = 0; nums.size(); j++)
+        {
+            if (i != j && nums[i] == nums[j])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        if (isDuplicate)
+        {
+            bool repeat = false;
+            
+            for (int x = 0; x < duplicates.size(); x++)
+            {
+                if (duplicates[x] == nums[i])
+                {
+                    repeat = true;
+                    break;
+                }
+            }
+            
+            if (!repeat)
+            {
+                duplicates.push_back(nums[i]); 
+            }
+        }
+    }
+    return duplicates.size();
+}
+
+void printVector(const std::vector<int>& nums)
+{
+    std::cout << "{ ";
+    
+    for (int i = 0; i < nums.size(); i++)
+    {
+        std::cout << nums[i];
+        if (i < nums.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << " }";
+}
+
+
+// Question 2
+void setDeepValue(int ***p, int value);
+
+void setDeepValue(int ***p, int value)
+{
+    ***p = value;
+}
