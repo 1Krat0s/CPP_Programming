aacfb9e4cc06749fcdf3b43025c120dd3cc9312f
Justin B. <jbryant9059@floridapoly.edu>
2025-11-07 19:05:56 +0000

    Polymorphism notes

---
+++ 20_polymorphism/polymorphism.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+//TODO
+// Reload function for weapon and all derived classes
+// Create an inventory for the player (set of weapons)
+
+class Weapon // Abstract class
+{
+    private:
+
+    public:
+        virtual void use() = 0; // Pure virtual function
+        
+};
+
+class Gun : public Weapon
+{
+    private:
+
+    public:
+        void use() override
+        {
+            std::cout << "BANG!" << std::endl;
+        }
+        virtual void reload() 
+        {
+            std::cout << "Reloading!" << std::endl;
+        }
+};
+
+class MachineGun : public Gun
+{
+    private:
+
+    public:
+        int magazine = 20;
+
+        void reload() override
+        {
+            std::cout << "Reloading!" << std::endl;
+            magazine = 20;
+        }
+
+        void use() override
+        {
+            while (magazine-- > 0)
+            {
+                std::cout << "BANG!";
+            }
+            std::cout << '\n';
+            reload();
+        }
+        
+};
+
+class Bazooka : public Weapon
+{
+    private:
+
+    public:
+        void use() override
+        {
+            std::cout << "BOOM!" << std::endl;
+        }
+    };
+    
+    class Knife : public Weapon
+    {
+        private:
+        
+        public:
+        void use() override
+        {
+            std::cout << "SLASH!" << std::endl;
+        }
+};
+
+
+class Player
+{
+    private:
+
+    public:
+        void use_weapon(Weapon& weapon)
+        {
+            weapon.use();
+        }
+};
+
+int main()
+{
+    Gun gun;
+    MachineGun mg;
+    Bazooka bazooka;
+    Knife knife;
+
+    gun.use();
+    // mg.shoot();
+
+    // Creare a reference to a base class
+    Gun& weapon = mg;
+    weapon.use();
+
+    std::cout << "------------\n";
+    Player player;
+    player.use_weapon(knife);
+
+
+    return 0;
+}
